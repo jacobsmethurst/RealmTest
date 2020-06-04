5c0023e07f5607a4c4e1a6435f509d33bc85eb99
Gyasi Story <gyasistory@gmail.com>
2020-06-04 15:25:55 -0400

    Gyasi's Suggestions

---
+++ app/src/main/java/com/example/realmtest/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.example.realmtest
+
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // TODO:  Add Realm Configuration here
+        // https://realm.io/docs/kotlin/latest/#configuring-a-realm
+    }
+}
---
+++ app/src/main/java/com/example/realmtest/model/Movie.kt
@@ -0,0 +1,3 @@
+package com.example.realmtest.model
+
+// TODO: Move Movie Class to this location
